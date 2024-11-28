beea3016cd74aa5271cd70d6b2194b30146f4b81
AnsonSavage <ansonsavage@gmail.com>
2024-11-27 18:35:48 -0700

    Done

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -9,7 +9,6 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --qos=dw87
 
 
 export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
@@ -21,4 +20,4 @@ echo "(Number of CPUs)"
 
 grep -c ^processor /proc/cpuinfo
 
-./blender-4.3.0-linux-x64/blender ./input_scenes/lone-monk_cycles_and_exposure-node_demo.blend --background --python ./src/main.py -- --start_seed=30000 --hdri_dir=./hdris/ --output_dir=./training_data/test_1/
+./blender-4.3.0-linux-x64/blender ./input_scenes/lone-monk_cycles_and_exposure-node_demo.blend --background --python ./src/main.py -- --start_seed=31000 --hdri_dir=./hdris/ --output_dir=./training_data/test_1/
