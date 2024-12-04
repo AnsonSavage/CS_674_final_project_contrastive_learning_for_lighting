60dd2436d9a70c00e1276325d6ce6bbf772865a2
AnsonSavage <ansonsavage@gmail.com>
2024-12-04 14:24:08 -0700

    Add file extension parameter to ContrastiveHDRIDataset and update image collection logic

--- src/datasets/contrastive_hdri_complete_sample_dataset.py
+++ src/datasets/contrastive_hdri_complete_sample_dataset.py
@@ -4,10 +4,10 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
-from ..utils.training_data_utils import extract_scene_name, extract_hdri_name
+from utils.training_data_utils import extract_scene_name, extract_hdri_name
 
 class ContrastiveHDRIDataset(Dataset):
-    def __init__(self, image_folder, scene_name, image_height=256, image_width=256, total_batches=1000000):
+    def __init__(self, image_folder, scene_name, image_height=256, image_width=256, total_batches=1000000, extension='.png'):
         """
         Initialize the ContrastiveHDRIDataset.
 
@@ -16,34 +16,42 @@ class ContrastiveHDRIDataset(Dataset):
             scene_name (str): Name of the scene to filter images.
             image_height (int, optional): Height of the images. Defaults to 256.
             image_width (int, optional): Width of the images. Defaults to 256.
-            total_batches (int, optional): Total number of batches. Defaults to 1000000. (This is because dataset length is required but getitem is independent of idx and new values are randomly sampled each time)
+            total_batches (int, optional): Total number of batches. Defaults to 1000000.
+            extension (str, optional): File extension to include. Defaults to '.png'.
         """
         self.image_folder = image_folder
         self.scene_name = scene_name
+        self.extension = extension.lower()
         
-        # Collect all image paths
-        self.images = [f for f in os.listdir(image_folder) if scene_name == extract_scene_name(f)]
-        assert len(self.images) > 0, f"No images found for scene {scene_name}"
+        # Collect all image paths with the specified extension
+        self.images = [
+            f for f in os.listdir(image_folder)
+            if f.lower().endswith(self.extension) and scene_name == extract_scene_name(f)
+        ]
+        assert len(self.images) > 0, f"No images found for scene {scene_name} with extension {self.extension}"
         
         # Group images by HDRI
         self.hdri_to_images = {}
         for img_name in self.images:
             hdri_name = extract_hdri_name(img_name)
-            if (hdri_name not in self.hdri_to_images):
+            if hdri_name not in self.hdri_to_images:
                 self.hdri_to_images[hdri_name] = []
             self.hdri_to_images[hdri_name].append(img_name)
         
         # Prepare list of HDRI names
         self.hdri_names = list(self.hdri_to_images.keys())
-        # For each key, assert that the number of images is equal
-        assert all(len(self.hdri_to_images[hdri_name]) == len(self.hdri_to_images[self.hdri_names[0]]) for hdri_name in self.hdri_names), "Number of images per HDRI is not equal"
+        # Ensure equal number of images per HDRI
+        assert all(
+            len(self.hdri_to_images[hdri_name]) == len(self.hdri_to_images[self.hdri_names[0]])
+            for hdri_name in self.hdri_names
+        ), "Number of images per HDRI is not equal"
 
         self.num_hdris = len(self.hdri_names)
         
         # Define the transformation to convert images to tensors
         self.transform = transforms.ToTensor()
         self.image_height, self.image_width = image_height, image_width
-        self.total_batches = total_batches  # Added line
+        self.total_batches = total_batches
         
     def __len__(self):
         """
